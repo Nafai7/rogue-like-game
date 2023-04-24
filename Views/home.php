1beb89a0facfe62590893c692a2c963601696678
Nafai7 <nafailuet@gmail.com>
2023-04-24 19:44:02 +0200

    Added font file and style file, added style loading to head. Created basic layout with borders.

--- Views/home.php
+++ Views/home.php
@@ -1,3 +1,16 @@
 <main>
-    <h1>HOME</h1>
+    <div id="game-container">
+        <div id="top-half">
+            <div id="screen">
+            </div>
+            <div id="character">
+            </div>
+        </div>
+        <div id="bottom-half">
+            <div id="actions">
+            </div>
+            <div id="story">
+            </div>
+        </div>
+    </div>
 </main>
